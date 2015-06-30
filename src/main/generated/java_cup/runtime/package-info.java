dd54428b713f5c2d32e1b4b2e02bd06abea7c54d
Vasiliy Pisar <daywalker@yandex-team.ru>
2015-06-30 16:58:04 +0300

    Updating generated lexer and parser

---
+++ src/main/generated/java_cup/runtime/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Due to ??xleft and ??xright being protected fields in ComplexSymbol class and because
+ * they are used by a CUP-parser directly, we need to place our Parser class in package
+ * 'java_cup.runtime'.
+ */
+package java_cup.runtime;
